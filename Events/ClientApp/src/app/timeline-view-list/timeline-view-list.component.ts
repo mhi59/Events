868d79ee6240612264a2a3d2c29ab555d5cefc50
mhhi59 <mhi59000@gmail.com>
2018-11-10 10:14:56 +0100

    travail sur la timeline

--- Events/ClientApp/src/app/timeline-view-list/timeline-view-list.component.ts
+++ Events/ClientApp/src/app/timeline-view-list/timeline-view-list.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { DataService } from './../service/dataService';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal/';
 import { Component, OnInit, Input, TemplateRef, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Model } from '../model/model-event';
 
 @Component({
   selector: 'app-timeline-view-list',
@@ -25,12 +26,8 @@ export class TimelineViewListComponent implements OnInit {
   @Input() info: string;
   @Input() id: string;
   @Input() index: number;
-  @Input() sortedArray: any[];
+  @Input() sortedArrayy: any[];
 
-  @Input() set themeActivated(theme: string) {  // On reçoit le themeSelected du composant parent eventViewComponent
-    this._themeActivated = theme;
-  }
-  get themeActivated(): string {return this._themeActivated; }  
 
   modalRef: BsModalRef;
   messageConfirmation = '';
@@ -38,7 +35,7 @@ export class TimelineViewListComponent implements OnInit {
   constructor(private dataService: DataService, private router: Router, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.jsonEvent = {
+  /*  this.jsonEvent = {
       'title': {
         'text': { 
           'headline': 'Les Events ' + this.sortedArray[0].theme.toUpperCase(),
@@ -47,8 +44,21 @@ export class TimelineViewListComponent implements OnInit {
       'events': [
       ]
     };
+*/setTimeout(() => {
+  console.log(this.sortedArrayy.length)
+  console.log(this.sortedArrayy)
+}, 3000);
+    
+    /*for (let i = 0, j = 0; i < this.sortedArrayy.length; i++)
+    {
+      while (j < this.sortedArrayy[i].length)
+      {
+        console.log(this.sortedArrayy[i][j]);
+        j++;
+      }
+    }
 
-    for(const event of this.sortedArray) {
+  /*  for(const event of this.sortedArray) {
       const jsonObject = {
         'start_date': {
           'month': new Date(event.date).getMonth(),
@@ -68,6 +78,6 @@ export class TimelineViewListComponent implements OnInit {
       const s = this.renderer.createElement('script'); // On crée le script à injecter pour ,instancier  la timeline
       s.type = 'text/javascript';
       s.text = 'timeline = new TL.Timeline(\'timeline-embed\', ' + timeline_json + ');'; // On instancie la timeline avec la chaîne JSON à exploiter
-      this.renderer.appendChild(this.timeline.nativeElement, s); // On injecte le script sur la page 
+      this.renderer.appendChild(this.timeline.nativeElement, s); // On injecte le script sur la page  */
   }
 }
